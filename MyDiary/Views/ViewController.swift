cd71a1ac5cecc4c95a6a543deb3aed300b09b1f7
Vo Minh Don <vominhdona1@gmail.com>
2022-01-11 11:45:31 +0700

    feat: create navigation component and main view

--- MyDiary/Views/ViewController.swift
+++ MyDiary/Views/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: BaseViewController {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var picker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let backgoundTapped = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped(_:)))
+        backgroundImage.isUserInteractionEnabled = true
+        backgroundImage.addGestureRecognizer(backgoundTapped)
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func dateClicked(_ sender: UIDatePicker) {
+        print(sender.date)
+    }
+    @objc func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        let alert = UIAlertController(title: "", message: "Cover photo", preferredStyle: .actionSheet)
 
+        alert.addAction(UIAlertAction(title: "Choose from Camera Roll", style: .default , handler:{ (UIAlertAction) in
+        }))
 
+        alert.addAction(UIAlertAction(title: "Choose from Diary", style: .default , handler:{ (UIAlertAction) in
+        }))
+
+        alert.addAction(UIAlertAction(title: "Delete", style: .default , handler:{ (UIAlertAction) in
+        }))
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:{ (UIAlertAction) in
+        }))
+
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
